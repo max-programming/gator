5521a9981ee741c3760fcda076d6f2495e5ce740
Usman S. <ui.sabuwala@gmail.com>
2025-09-07 13:36:15 +0000

    Refactor main.go to implement command handling and improve error logging. Added state and command structures, registered login command, and replaced panic with log.Fatal for error handling.

--- main.go
+++ main.go
@@ -2,21 +2,72 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/max-programming/gator/internal/config"
 )
 
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	cmds map[string]func(*state, command) error
+}
+
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	cfg.SetUser("usman")
+	s := state{&cfg}
+	cmds := commands{
+		make(map[string]func(*state, command) error),
+	}
 
-	cfg, err = config.Read()
+	cmds.register("login", handlerLogin)
+
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatal("no command provided")
+	}
+
+	cmdName := args[1]
+	cmd := command{name: cmdName, args: args[2:]}
+
+	err = cmds.run(&s, cmd)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	fmt.Println(cfg)
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if cmd.name != "login" {
+		return fmt.Errorf("invalid command")
+	}
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("username is required")
+	}
+	username := cmd.args[0]
+	err := s.cfg.SetUser(username)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Logged in as %s\n", username)
+	return nil
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	return c.cmds[cmd.name](s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.cmds[name] = f
 }
