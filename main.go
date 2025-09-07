d8b33bf90c2fc13c15ea12e5bfba5f448898cf7d
Usman S. <ui.sabuwala@gmail.com>
2025-09-07 14:15:46 +0000

    implement register handler, db connections, and login handler

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/max-programming/gator/internal/config"
+	"github.com/max-programming/gator/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -22,17 +30,29 @@ type commands struct {
 }
 
 func main() {
+
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	s := state{&cfg}
+	db, err := sql.Open("postgres", cfg.DBUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbQueries := database.New(db)
+
+	s := state{
+		db:  dbQueries,
+		cfg: &cfg,
+	}
 	cmds := commands{
 		make(map[string]func(*state, command) error),
 	}
 
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handleRegister)
 
 	args := os.Args
 	if len(args) < 2 {
@@ -48,6 +68,37 @@ func main() {
 	}
 }
 
+func handleRegister(s *state, cmd command) error {
+	if cmd.name != "register" {
+		return fmt.Errorf("invalid command")
+	}
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("username is required")
+	}
+	username := cmd.args[0]
+	user, err := s.db.CreateUser(
+		context.Background(),
+		database.CreateUserParams{
+			ID:        uuid.New(),
+			Name:      username,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+	)
+	if err != nil {
+		return err
+	}
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return err
+	}
+	fmt.Printf(
+		"Logged in as %s\nID: %s\nCreated At: %s\nUpdated At: %s",
+		user.Name, user.ID, user.CreatedAt.Local().String(), user.UpdatedAt.Local().String(),
+	)
+	return nil
+}
+
 func handlerLogin(s *state, cmd command) error {
 	if cmd.name != "login" {
 		return fmt.Errorf("invalid command")
@@ -56,7 +107,11 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("username is required")
 	}
 	username := cmd.args[0]
-	err := s.cfg.SetUser(username)
+	user, err := s.db.GetUser(context.Background(), username)
+	if err != nil {
+		return err
+	}
+	err = s.cfg.SetUser(user.Name)
 	if err != nil {
 		return err
 	}
