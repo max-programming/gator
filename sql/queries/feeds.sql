75abc08c52262b9eb5f22bc5323e9509d6dcfa23
Usman S. <ui.sabuwala@gmail.com>
2025-09-07 15:56:55 +0000

    Added a GetFeeds query

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -2,3 +2,10 @@
 INSERT INTO feeds (id, name, url, user_id, created_at, updated_at)
 VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
+
+-- name: GetFeeds :many
+SELECT f.name,
+  f.url,
+  u.name AS username
+FROM feeds f
+  JOIN users u ON u.id = f.user_id;
